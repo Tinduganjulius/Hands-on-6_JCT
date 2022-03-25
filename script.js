bb74b30b5e509cb5312f21145432b71407377e0a
JCT000 <juliustindugan14@gmail.com>
2022-03-25 22:57:10 +0800

    hans on

---
+++ script.js
@@ -0,0 +1,5 @@
+var name = window.prompt("What is your name?");
+var section = window.prompt("What is your section?");
+var grade = window.prompt("What is your target grade for this semester?");
+
+document.write ("Mabuhay! " + name + ", you are looking great. Welcome to " + section + " the best section ever. I promise to to do my best just to get " + grade + " for this semester.");
